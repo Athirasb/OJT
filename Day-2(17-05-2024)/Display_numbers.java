6ad19298fb632ea323e4de242ce618164be58ad4
Athirasb <122441557+Athirasb@users.noreply.github.com>
2024-05-17 17:04:53 +0530

    Add files via upload

---
+++ Day-2(17-05-2024)/Display_numbers.java
@@ -0,0 +1,16 @@
+//Display Numbers from 1 to 5
+// Program to display numbers from 1 to 5
+public class Display_numbers {
+    public static void main(String[] args) {
+
+        // declare variables
+        int i = 1, n = 5;
+
+        // while loop from 1 to 5
+        while(i <= n) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+
---
+++ Day-2(17-05-2024)/Sumofpositivenumbers.java
@@ -0,0 +1,24 @@
+// Sum of Positive Numbers using do while loop
+import java.util.Scanner;
+
+public class Sumofpositivenumbers {
+    public static void main(String[] args) {
+        int sum = 0;
+        int number;
+
+        Scanner input = new Scanner(System.in);
+
+        do {
+            System.out.println("Enter a number (enter a negative number to stop):");
+            number = input.nextInt();
+
+            if (number >= 0) {
+                sum += number;
+            }
+        } while (number >= 0);
+
+        System.out.println("Sum = " + sum);
+        input.close();
+    }
+}
+
